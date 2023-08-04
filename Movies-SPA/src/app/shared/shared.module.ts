870a2df313c218187f794b2013aac8fd600e222e
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-08-04 23:15:26 +0300

    added directive

--- Movies-SPA/src/app/shared/shared.module.ts
+++ Movies-SPA/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { PasswordsDirective } from './validators/passwords.directive';
 
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
+    PasswordsDirective,
     
   ],
   imports: [
---
+++ Movies-SPA/src/app/shared/validators/passwords.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PasswordsDirective } from './passwords.directive';
+
+describe('PasswordsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PasswordsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
