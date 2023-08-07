a21c16289880ef71ea42c219effdc35098f68fea
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-08-07 19:30:33 +0300

    making profile page dynamic

--- Movies-SPA/src/app/user/profile/profile.component.ts
+++ Movies-SPA/src/app/user/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Movie } from 'src/app/types/movie';
+import { Profile } from 'src/app/types/userProfile';
+
 
 @Component({
   selector: 'app-profile',
@@ -9,17 +11,46 @@ import { Movie } from 'src/app/types/movie';
 })
 export class ProfileComponent implements OnInit {
 
+
+  
+  profileDetails: Profile  = {
+    username: '',
+    email: '',
+  };
+ 
+
   bookmarkedMovies: Movie[] = [];
+  errorMessage: string | null = null; 
 
   constructor( private authService: AuthService) { }
 
 
-
-
+  
+  
   ngOnInit(): void {
-    this.authService.getUserBookmarks().subscribe((moviesData) =>{
-      console.log(moviesData)
-    })
+    this.authService.getUserBookmarks().subscribe(
+      (moviesData) =>{
+        this.bookmarkedMovies = moviesData;
+        this.errorMessage = null;
+        
+        if (this.bookmarkedMovies.length === 0) {
+          this.errorMessage = 'No movies have been bookmarked yet!';
+        }
+        const { username, email } = this.authService.user!;
+        this.profileDetails =  { username, email };
+
+      console.log('Error =>', this.errorMessage)
+      console.log('Moives =>', this.bookmarkedMovies)
+     
+    },
+    (error) => {
+      this.errorMessage = error;
+      this.bookmarkedMovies = [];
+      console.log('Error occured:', this.errorMessage)
+
+    }
+    
+    )
     
   }
 }
