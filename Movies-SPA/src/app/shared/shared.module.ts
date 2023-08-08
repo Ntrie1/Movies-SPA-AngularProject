e2e0214e20196e74bb3cad13bdd08975a4806abd
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-08-08 17:14:10 +0300

    pipe added

--- Movies-SPA/src/app/shared/shared.module.ts
+++ Movies-SPA/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { SlicePipe } from './pipes/slice.pipe';
 
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
+    SlicePipe,
     
   ],
   imports: [
