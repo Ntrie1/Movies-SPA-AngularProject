13032e09c25a90bd0269f00847c5d8000e06eaf4
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-08-08 21:44:29 +0300

    deleted unnecessary code

--- Rest-API/router/movies.js
+++ Rest-API/router/movies.js
@@ -75,36 +75,6 @@ router.put('/:movieId/bookmark',auth(), async (req, res)=>{
 
 })
 
-// router.put('/:movieId/bookmark', authMiddleware, async (req, res) => {
-//     const { movieId } = req.params;
-//     const { _id: userId } = req.user;
-  
-//     try {
-//       const user = await userModel.findById(userId);
-  
-//       if (!user) {
-//         return res.status(404).json({ error: 'User not found' });
-//       }
-  
-//       // Check if the movie is in the user's bookmarks
-//       const movieIndex = user.movies.findIndex((movie) => movie._id.toString() === movieId);
-//       if (movieIndex === -1) {
-//         return res.status(404).json({ error: 'Movie not bookmarked by the user' });
-//       }
-  
-//       // Remove the movie from the bookmarks
-//       user.movies.splice(movieIndex, 1);
-  
-//       // Save the updated user object
-//       await user.save();
-  
-//       res.status(200).json({ message: 'Movie removed from bookmarks successfully' });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Server error' });
-//     }
-//   });
-
 
   router.put('/removeBookmark', auth(), async (req, res) => {
     const { movieId } = req.body; // Assuming the movieId is sent in the request body
