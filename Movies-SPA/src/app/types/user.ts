8488425871c9648295ed165c7b40df2bd669b67b
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-08-10 13:07:27 +0300

    fixed interface

--- Movies-SPA/src/app/types/user.ts
+++ Movies-SPA/src/app/types/user.ts
@@ -1,7 +1,10 @@
+import { Movie } from "./movie";
+
 export interface User{
     username:string,
     email: string,
     password: string,
     rePassword: string,
-    _id: string
+    _id: string,
+    movies: Movie[],
 }
